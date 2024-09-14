dec06242baca8aac1106f4dddaddf1d8924c0201
Jhonatan Muniz <jhonatanmuniz123@gmail.com>
2024-09-13 22:05:03 -0300

    add tests create attachments

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,5 +1,6 @@
 import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { CreateQuestionUseCase } from './create-question'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository,
@@ -29,6 +30,28 @@ describe('Create Question', () => {
     expect(result.isSuccess()).toBe(true)
     expect(inMemoryQuestionsRepository.items[0].id).toEqual(
       result.value?.question.id,
+    )
+  })
+
+  it('should persist attachments when creating a new question', async () => {
+    const result = await sut.execute({
+      authorId: '1',
+      title: 'Nova pergunta',
+      content: 'criando pergunta',
+      attachmentsIds: ['1', '2'],
+    })
+
+    expect(result.isSuccess()).toBe(true)
+    expect(inMemoryQuestionAttachmentsRepository.items).toHaveLength(2)
+    expect(inMemoryQuestionAttachmentsRepository.items).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          attachmentId: new UniqueEntityID('1'),
+        }),
+        expect.objectContaining({
+          attachmentId: new UniqueEntityID('2'),
+        }),
+      ]),
     )
   })
 })
